24bdcb6660426f553a1d1a1626a91b0d3ce33788
miguelcastiyo <castillo_m17@icloud.com>
2026-04-09 20:15:25 -0700

    cleanup

--- scripts/pandoc-filters/strip-first-heading.lua
+++
@@ -1,9 +0,0 @@
--- Removes the first level-1 heading from the document body.
-local seen = false
-
-function Header(el)
-  if not seen and el.level == 1 then
-    seen = true
-    return {}
-  end
-end
